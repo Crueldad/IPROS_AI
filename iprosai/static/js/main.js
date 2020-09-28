75dec0eea8a470502eaf01577a170eeaa4c2ccb4
Crueldad <55165234+Crueldad@users.noreply.github.com>
2020-09-28 15:17:58 -0700

    added overall search function

--- iprosai/static/js/main.js
+++ iprosai/static/js/main.js
@@ -20,37 +20,7 @@
 		$(table2).find("."+column).removeClass('hov-column-'+ verTable);
 		$(table1).find(".row100.head ."+column).removeClass('hov-column-head-'+ verTable);
 	});
-	
-	const healthbody = document.querySelector("#health_table > tbody");
-	var data = JSON.parse("{{sql_query|escapejs}}");
-
-	window.onload = (event) => {
-		populatehealth(data);
-		
-	};
-
-	function populatehealth (data) {
-		while (healthbody.firstChild) {
-			healthbody.removeChild(healthbody.firstChild);
-	} 
-		var num = 1;
-		data.forEach((row) => {
-			const tr = document.createElement("tr");
-			tr.setAttribute('class', 'row100');
-			row.forEach((cell) =>{
-				const td = document.createElement("td");
-				td.setAttribute('class', 'column100 column1');
-				td.setAttribute('data-column', 'column1');
-				num += 1
-				td.textContent = cell;
-				tr.appendChild(td);
-			
-			});
-			healthbody.appendChild(tr);
-
-		});
-
-}
+    
 
 })(jQuery);
 
